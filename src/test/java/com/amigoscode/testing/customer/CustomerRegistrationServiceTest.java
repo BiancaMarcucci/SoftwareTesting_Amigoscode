056fd2655b179b78c1cf712144a38f009c0d56fe
Bianca <marcucci.bianca96@gmail.com>
2021-09-14 14:12:02 +0100

    ..

--- src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
@@ -10,10 +10,12 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 import java.util.UUID;
 
+import static net.bytebuddy.matcher.ElementMatchers.any;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 
 class CustomerRegistrationServiceTest {
 
@@ -50,5 +52,26 @@ class CustomerRegistrationServiceTest {
         then(customerRepository).should().save(customerArgumentCaptor.capture());
         Customer customerArgumentCaptorValue=customerArgumentCaptor.getValue();
         assertThat(customerArgumentCaptorValue).isEqualToComparingFieldByField(customer);
+    }
+
+    @Test
+    void itShouldNotRegisterNewCustomerWhenCustomerAlreadyExists() {
+        //GIVEN a phone number and request
+        String phoneNumber = "000099";
+        UUID id=UUID.randomUUID();
+        Customer customer=new Customer(id,"Maryam",phoneNumber);
+
+        //... a request
+        CustomerRegistrationRequest request=new CustomerRegistrationRequest(customer);
+
+        // to make optional (inside mocked customerRepository) be empty, to later test if we can register a customer with new phone number
+        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber)).willReturn(Optional.of(customer));
+
+        //WHEN
+        underTest.registerNewCustomer(request);
+
+        //THEN
+        then(customerRepository).should().selectCustomerByPhoneNumber(phoneNumber);
+        then(customerRepository).shouldHaveNoMoreInteractions();
     }
 }
