b8fbffdf54dc69515abbb0e325542a91f708bfec
Bianca <marcucci.bianca96@gmail.com>
2021-09-14 10:03:37 +0100

    ..

--- src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
@@ -1,12 +1,20 @@
 package com.amigoscode.testing.customer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerRegistrationServiceTest {
 
+    @Mock
+    private CustomerRepository customerRepository;
+    private CustomerRegistrationService underTest;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        underTest=new CustomerRegistrationService(customerRepository);
     }
 }
